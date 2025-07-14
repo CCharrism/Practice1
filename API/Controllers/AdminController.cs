3b9c24ab8af9c67b5ab0b666d35f4846a7d0c7f0
HARRIS MAIZAN <HARRISMAIZAN@BAGH.MTBC.COM>
2025-07-14 06:37:26 -0400

    Getting Users with roles via policy based authentication

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,18 +1,31 @@
 using System;
+using API.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AdminController : BaseApiController
+public class AdminController(UserManager<AppUser> userManager) : BaseApiController
 {
 
     [Authorize(Policy = "RequireAdminRole")]
     [HttpGet("users-with-roles")]
-    public ActionResult GetUsersWithRoles()
+    public async Task<ActionResult> GetUsersWithRoles()
     {
+        var users = await userManager.Users
+            .Include(u => u.UserRoles)
+            .ThenInclude(ur => ur.Role)
+            .OrderBy(u => u.UserName)
+            .Select(u => new
+            {
+                u.Id,
+                UserName = u.UserName,
+                Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
+            }).ToListAsync();
         // This is a placeholder for admin-specific actions
-        return Ok("Admin data accessed successfully.");
+        return Ok(users);
     }
     
     [Authorize (Policy = "ModeratePhotoRole")]
